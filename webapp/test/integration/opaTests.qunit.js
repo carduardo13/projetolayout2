f8b98e16579b3e8bad1f78e0a030b207a0d63da7
Carlos Eduardo <carloseduardobs98@gmail.com>
2023-02-06 13:55:43 +0000

    Initial Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["projetolayout2/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"projetolayout2/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
